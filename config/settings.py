84614b5fe4e4a6c7ef242011a687a39019767a4b
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-09-29 11:27:10 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
+    'master',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
