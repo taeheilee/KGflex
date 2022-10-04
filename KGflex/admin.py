14a844fe47b75a6aa9d95789b85b5fecc007ef41
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-04 13:11:11 +0900

    Delete KGflex directory

--- KGflex/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from KGflex.models import KGflex, Notice, KDrama, entertainment, KMovie, UMovie
-# Register your models here.
-admin.site.register(KGflex)
-admin.site.register(Notice)
-admin.site.register(KDrama)
-admin.site.register(KMovie)
-admin.site.register(UMovie)
-admin.site.register(entertainment)
