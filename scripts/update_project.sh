0612b4de53948fe2edeba31703327bd4f9c7fafb
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-19 03:33:07 +0900

    Update update_project.sh

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-killall python manage.py runserver 0:8000
 cd /root/KGflex
 \cp -rf config media static KGflex templates requirements.txt /django
 cd /django
@@ -13,6 +12,3 @@ sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"mydb-ins
 sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"mydb-instance-1-ap-northeast-2a.c7tpfyaiw1nc.ap-northeast-2.rds.amazonaws.com\"/" /django/config/settings.py
 sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"redis-001-001.vo2lks.ng.0001.apn2.cache.amazonaws.com\"/" /django/config/settings.py
 
-nohup python manage.py runserver 0:8000&
-
-run.sh >/dev/null 2>&1 &
