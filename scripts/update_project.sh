2506c3d3f68cae7b843564c4a8e20eeeae9f6398
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-18 18:43:18 +0900

    Update update_project.sh

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -7,10 +7,8 @@ pip install -r requirements.txt
 
 sudo sed -i "s/DEBUG = .*/DEBUG = False/" /KGflex/config/settings.py
 sudo sed -i "s/is_Production = .*/is_Production = False/" /KGflex/config/settings.py
-sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /KGflex
-/config/settings.py
-sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /KGflex/
-config/settings.py
+sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /KGflex/config/settings.py
+sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /KGflex/config/settings.py
 sudo sed -i "s/USE_CACHE = .*/USE_CACHE = False/" /KGflex/config/settings.py
 sudo sed -i "s/IS_HOME = .*/IS_HOME = False/" /KGflex/config/settings.py
 sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"{REDIS_HOST}\"/" /KGflex/config/settings.py
