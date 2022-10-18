f3a17fd4a687c3bddc5feb4c2471f5dc827a924a
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-19 03:48:10 +0900

    Update validation.sh

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1/)
+result=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8000/)
 if [[ "$result" =~ "200" ]]; then
   exit 0
 else
