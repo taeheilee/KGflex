9fdd77e2261a648db57ed46ee8b6a91148dd621b
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-19 03:18:02 +0900

    Update update_project.sh

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -9,8 +9,10 @@ sudo sed -i "/twisted-iocpsupport==1.0.2/d" /django/requirements.txt
 pip install -r requirements.txt
 
 sudo sed -i "s/DEBUG = .*/DEBUG = False/" /django/config/settings.py
-sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /django/config/settings.py
-sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /django/config/settings.py
-sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"{REDIS_HOST}\"/" /django/config/settings.py
+sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"mydb-instance-1.c7tpfyaiw1nc.ap-northeast-2.rds.amazonaws.com\"/" /django/config/settings.py
+sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"mydb-instance-1-ap-northeast-2a.c7tpfyaiw1nc.ap-northeast-2.rds.amazonaws.com\"/" /django/config/settings.py
+sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"redis-001-001.vo2lks.ng.0001.apn2.cache.amazonaws.com\"/" /django/config/settings.py
 
 nohup python manage.py runserver 0:8000&
+
+run.sh >/dev/null 2>&1 &
