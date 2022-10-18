1a333afb28584f6c0cc9a0760ff3609153fbb238
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-19 00:14:55 +0900

    Update update_project.sh

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-cd /KGflex
+cd /
+sudo cp -rf /KGflex /django
+cd /django
 source myhome/bin/activate
-sudo sed -i "/twisted-iocpsupport==1.0.2/d" /KGflex/requirements.txt
+sudo sed -i "/twisted-iocpsupport==1.0.2/d" /django/requirements.txt
 pip install -r requirements.txt
 
-sudo sed -i "s/DEBUG = .*/DEBUG = False/" /KGflex/config/settings.py
-sudo sed -i "s/is_Production = .*/is_Production = False/" /KGflex/config/settings.py
-sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /KGflex/config/settings.py
-sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /KGflex/config/settings.py
-sudo sed -i "s/USE_CACHE = .*/USE_CACHE = False/" /KGflex/config/settings.py
-sudo sed -i "s/IS_HOME = .*/IS_HOME = False/" /KGflex/config/settings.py
-sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"{REDIS_HOST}\"/" /KGflex/config/settings.py
-sudo systemctl restart KGflex
+sudo sed -i "s/DEBUG = .*/DEBUG = False/" /django/config/settings.py
+sudo sed -i "s/is_Production = .*/is_Production = False/" /django/config/settings.py
+sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /django/config/settings.py
+sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /django/config/settings.py
+sudo sed -i "s/USE_CACHE = .*/USE_CACHE = False/" /django/config/settings.py
+sudo sed -i "s/IS_HOME = .*/IS_HOME = False/" /django/config/settings.py
+sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"{REDIS_HOST}\"/" /django/config/settings.py
+sudo systemctl restart django
 source myhome/bin/activate
 nohup python manage.py runserver 0:8000 &
