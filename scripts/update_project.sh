8a2b8b371a407b4047383c3e076d7e4c96139a4e
taeheilee <113228649+taeheilee@users.noreply.github.com>
2022-10-18 18:23:41 +0900

    Update update_project.sh

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /root/KGflex
 
-\cp -rf config MTV common templates requirements.txt /KGflex
+\cp -rf config KGflex common templates requirements.txt /KGflex
 
 cd /KGflex
 source myhome/bin/activate
-sudo sed -i "/twisted-iocpsupport==1.0.2/d" /my-django/requirements.txt
+sudo sed -i "/twisted-iocpsupport==1.0.2/d" /KGflex/requirements.txt
 pip install -r requirements.txt
 
-sudo sed -i "s/DEBUG = .*/DEBUG = False/" /my-django/config/settings.py
-sudo sed -i "s/is_Production = .*/is_Production = False/" /my-django/config/settings.py
+sudo sed -i "s/DEBUG = .*/DEBUG = False/" /KGflex/config/settings.py
+sudo sed -i "s/is_Production = .*/is_Production = False/" /KGflex/config/settings.py
 sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /KGflex
 /config/settings.py
 sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /KGflex/
